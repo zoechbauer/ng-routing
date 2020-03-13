7403b0403cb87e777fbb049e489164c810508009
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-13 18:26:21 +0100

    passing parameters, querystring and fragement in template and typescript code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,9 +11,12 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {}
 
-  onLoadServers() {
+  onLoadServers(id: number) {
     // perform some calculations
 
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers', id, 'edit'], {
+      queryParams: { allowEdit: '1' },
+      fragment: 'loading'
+    });
   }
 }
