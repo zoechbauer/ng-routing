530f7985258a0c8e84c0104ea44648ddeb6c1c0b
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-13 15:44:37 +0100

    using relative paths in programmatically navigation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, Route, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,9 +10,18 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   servers: { id: number; name: string; status: string }[] = [];
 
-  constructor(private serversService: ServersService) {}
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    //     this statement would produce the route localhost:4200/servers/servers
+    // this.router.navigate(['servers'], { relativeTo: this.route });
   }
 }
