09276bffd194ab47a3bc65604206db64bab6262e
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-14 13:20:59 +0100

    Redirection and Wildcards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -26,7 +27,10 @@ const appRoutes: Routes = [
       { path: ':id/edit', component: EditServerComponent },
       { path: ':id', component: ServerComponent }
     ]
-  }
+  },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: 'not-found' }
+  // { path: '', redirectTo: 'not-found', pathMatch: 'full' }
 ];
 @NgModule({
   declarations: [
@@ -36,7 +40,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [ServersService],
